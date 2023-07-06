3497af268138028f05c150f4140d76cb0138c8e7
imakero <45946863+imakero@users.noreply.github.com>
2023-07-06 13:17:29 +0200

    initial commit

---
+++ topo-api/app/core/models.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.models import AbstractUser
+
+from django.contrib.gis.db import models
+
+
+class CustomUser(AbstractUser):
+    pass
---
+++ topo-api/app/core/urls.py
@@ -0,0 +1,4 @@
+from django.urls import path
+from core.views import index
+
+urlpatterns = [path("", index, name="index")]
