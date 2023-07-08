872288fdab64578ec68d43ec3d355903c6d3faed
imakero <45946863+imakero@users.noreply.github.com>
2023-07-08 11:19:36 +0200

    add tag model

--- topo-api/app/core/tests/test_models.py
+++ topo-api/app/core/tests/test_models.py
@@ -2,11 +2,11 @@ import pytest
 
 from django.contrib.gis.geos import fromstr
 
-from core.models import Climbable
+from core.models import Climbable, Tag
 
 
 @pytest.mark.django_db
-def test_climbable_model():
+def test_climbable():
     climbable = Climbable.objects.create(
         name="Svälthammaren",
         type="BL",
@@ -32,3 +32,10 @@ def test_climbable_gets_default_type_bl():
     )
     assert climbable.type == "BL"
     assert climbable.get_type_display() == "block"
+
+
+@pytest.mark.django_db
+def test_tag():
+    tag = Tag.objects.create(name="slab")
+    assert tag.name == "slab"
+    assert str(tag) == "slab"
