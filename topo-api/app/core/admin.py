d18990eb4b911230358c130d80d3763b528395a9
imakero <45946863+imakero@users.noreply.github.com>
2023-07-09 20:19:56 +0200

    add problem and tag to admin

--- topo-api/app/core/admin.py
+++ topo-api/app/core/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 from django.contrib.gis.admin import GISModelAdmin
 
-from .models import Climbable
+from .models import Climbable, Problem, Tag
 
 
 @admin.register(Climbable)
 class ClimbableAdmin(GISModelAdmin):
     list_display = ("name", "type", "location")
     search_fields = ("name",)
+
+
+@admin.register(Problem)
+class ProblemAdmin(admin.ModelAdmin):
+    list_display = ("name", "grade", "climbable")
+    search_fields = ("name", "climbable__name")
+
+
+admin.site.register(Tag)
