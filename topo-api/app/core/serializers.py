f261e190720a1ab15af3d223106e4be21340e86a
imakero <45946863+imakero@users.noreply.github.com>
2023-07-11 20:31:11 +0200

    add location to problem serializer

--- topo-api/app/core/serializers.py
+++ topo-api/app/core/serializers.py
@@ -1,9 +1,19 @@
-from rest_framework import serializers
+from rest_framework_gis import serializers
 
 from core.models import Problem
 
 
 class ProblemSerializer(serializers.ModelSerializer):
+    location = serializers.GeometryField(source="climbable.location")
+
     class Meta:
         model = Problem
-        fields = ["id", "name", "description", "grade", "climbable", "tags"]
+        fields = [
+            "id",
+            "name",
+            "description",
+            "grade",
+            "climbable",
+            "tags",
+            "location",
+        ]
