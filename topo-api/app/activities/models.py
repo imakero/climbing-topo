6b26e1a612a8dc75e7586865da9e2f8508edef79
imakero <45946863+imakero@users.noreply.github.com>
2023-07-18 14:17:50 +0200

    add ascent model

--- topo-api/app/activities/models.py
+++ topo-api/app/activities/models.py
@@ -1,1 +1,19 @@
 from django.db import models
+from shared.models import TimeStampModel
+
+
+class Ascent(TimeStampModel):
+    ZERO_TO_FIVE_CHOICES = (
+        (0, "Horrible"),
+        (1, "Ok"),
+        (2, "Good"),
+        (3, "Great"),
+        (4, "Amazing"),
+        (5, "Perfect"),
+    )
+    problem = models.ForeignKey("climbs.Problem", on_delete=models.CASCADE)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    given_rating = models.IntegerField(
+        choices=ZERO_TO_FIVE_CHOICES, blank=True, null=True
+    )
+    comment = models.TextField(max_length=1000, blank=True, default="")
