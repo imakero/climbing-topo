4471f7fda1d15957b02025cfb48c0a60fe6201b1
imakero <45946863+imakero@users.noreply.github.com>
2023-07-19 20:47:54 +0200

    make user read only

--- topo-api/app/activities/serializers.py
+++ topo-api/app/activities/serializers.py
@@ -5,4 +5,13 @@ from .models import Ascent
 class AscentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ascent
-        fields = ["id", "user", "problem", "comment", "given_rating"]
+        fields = [
+            "id",
+            "user",
+            "problem",
+            "comment",
+            "given_rating",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = ["user"]
