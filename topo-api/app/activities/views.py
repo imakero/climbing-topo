ab23ffc46f268c1366abc79a13b6a8bfc2a75635
imakero <45946863+imakero@users.noreply.github.com>
2023-07-19 20:48:37 +0200

    read user from request when logging ascent

--- topo-api/app/activities/views.py
+++ topo-api/app/activities/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.exceptions import NotAuthenticated
 from .models import Ascent
 from .serializers import AscentSerializer
 
@@ -6,6 +7,11 @@ from .serializers import AscentSerializer
 class AllAscentsListView(generics.ListCreateAPIView):
     queryset = Ascent.objects.all()
     serializer_class = AscentSerializer
+
+    def perform_create(self, serializer):
+        if not self.request.user.is_authenticated:
+            raise NotAuthenticated()
+        serializer.save(user=self.request.user)
 
 
 class AscentDetailView(generics.RetrieveUpdateDestroyAPIView):
