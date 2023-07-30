9b86e31709b612a034b4a499aab514875247a5ba
imakero <45946863+imakero@users.noreply.github.com>
2023-07-30 23:39:38 +0200

    annotate problem qs with ascents and rating

--- topo-api/app/climbs/views.py
+++ topo-api/app/climbs/views.py
@@ -1,5 +1,7 @@
 from django.contrib.gis.db.models.functions import Distance
 from django.contrib.gis.geos import fromstr
+from django.db.models import Count, Avg
+
 from rest_framework import generics
 
 from climbs.filters import ProblemFilter
@@ -10,22 +12,26 @@ from climbs.serializers import ProblemSerializer, TopoImageSerializer
 class ProblemsView(generics.ListCreateAPIView):
     serializer_class = ProblemSerializer
     filterset_class = ProblemFilter
+    queryset = (
+        Problem.objects.prefetch_related("tags")
+        .select_related("climbable")
+        .annotate(
+            ascents=Count("ascent"),
+            rating=Avg("ascent__given_rating"),
+        )
+    )
 
     def get_queryset(self):
         lon = self.request.query_params.get("lon", None)
         lat = self.request.query_params.get("lat", None)
 
         if lon is None or lat is None:
-            return Problem.objects.prefetch_related("tags").select_related(
-                "climbable"
-            )
+            return self.queryset
 
         point = fromstr(f"SRID=4326;POINT ({lon} {lat})")
 
-        return (
-            Problem.objects.prefetch_related("tags")
-            .select_related("climbable")
-            .annotate(dist_km=Distance("climbable__location", point) / 1000)
+        return self.queryset.annotate(
+            dist_km=Distance("climbable__location", point) / 1000,
         )
 
 
