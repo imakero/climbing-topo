40ea31d37d9f382604edb4d37292b70a93d89e23
imakero <45946863+imakero@users.noreply.github.com>
2023-07-30 12:45:12 +0200

    add ascent fixtures

--- topo-api/app/activities/tests/conftest.py
+++ topo-api/app/activities/tests/conftest.py
@@ -9,3 +9,17 @@ def add_ascent():
         return Ascent.objects.create(**kwargs)
 
     return _add_ascent
+
+
+@pytest.fixture
+def ascent(add_ascent, problem, user):
+    return add_ascent(
+        problem=problem, user=user, given_rating=4, comment="Yes!"
+    )
+
+
+@pytest.fixture
+def ascent_other(add_ascent, problem_other, user_other):
+    return add_ascent(
+        problem=problem_other, user=user_other, given_rating=3, comment="Knee!"
+    )
