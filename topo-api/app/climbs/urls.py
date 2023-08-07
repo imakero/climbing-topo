9ff994e45b8ebef14b40e2ef4aa414382c3d32d3
imakero <45946863+imakero@users.noreply.github.com>
2023-08-07 13:04:22 +0200

    add problem detail views

--- topo-api/app/climbs/urls.py
+++ topo-api/app/climbs/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from climbs.views import ProblemsView, TopoImagesView, TopoImageView
+from climbs.views import (
+    ProblemsView,
+    ProblemView,
+    TopoImagesView,
+    TopoImageView,
+)
 
 urlpatterns = [
     path("problems/", ProblemsView.as_view(), name="problems"),
+    path("problems/<int:pk>/", ProblemView.as_view(), name="problem"),
     path("topos/", TopoImagesView.as_view(), name="topos"),
     path("topos/<int:pk>/", TopoImageView.as_view(), name="topo"),
 ]
