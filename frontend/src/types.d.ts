5fac8851b3e478eb156f617132272d7d591786e9
imakero <45946863+imakero@users.noreply.github.com>
2023-08-17 15:40:21 +0200

    add types for basic models

--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -1,7 +1,35 @@
+type WithId<T> = T & { id: number };
+
 type User = {
   pk: number;
   username: string;
   email: string;
   firstName: string;
   lastName: string;
+};
+
+type Problem = {
+  name: string;
+  description: string;
+  grade: string;
+  location: WithId<TopoLocation>;
+  tags: string[];
+  ascents: number;
+  rating: number;
+};
+
+type TopoLocation = {
+  name: string;
+  type: string;
+  position: Position;
+};
+
+type Position = {
+  lon: number;
+  lat: number;
+  googleMapsString: string;
+};
+
+type Tag = {
+  name: string;
 };
