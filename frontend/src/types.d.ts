c2a30a66ea489a9e69f5322f599d2e05cd41efdd
imakero <45946863+imakero@users.noreply.github.com>
2023-08-27 23:43:43 +0200

    update LocationImage type

--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -31,12 +31,29 @@ type TopoLocation = {
   images: WithId<LocationImage>[];
 };
 
+type Line = {
+  locationImage: number;
+  problem: {
+    id: number;
+    name: string;
+    grade: string;
+  };
+  points: {
+    type: "LineString";
+    coordinates: [number, number][];
+  };
+};
+
 type LocationImage = {
-  location: Omit<TopoLocation, "images">;
+  location: Omit<TopoLocation, "images"> & { problems: LocationImageProblem[] };
   image: string;
   imageWidth: number;
   imageHeight: number;
+  lines: LocationImageLine[];
 };
+
+type LocationImageProblem = Pick<WithId<Problem>, "id" | "name" | "grade">;
+type LocationImageLine = Pick<WithId<Line>, "id" | "points" | "problem">;
 
 type Position = {
   lon: number;
