5acaf47712b967cb8353b08c5014bfe54c5bef0e
imakero <45946863+imakero@users.noreply.github.com>
2023-08-27 16:04:08 +0200

    extract auth api calls to library methods

---
+++ frontend/src/library/auth.ts
@@ -0,0 +1,25 @@
+export const login = async (username: string, password: string) => {
+  const response = await fetch("http://localhost:8009/api/v1/auth/login/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    body: JSON.stringify({ username, password }),
+  });
+  if (!response.ok) {
+    throw response;
+  }
+  return await response.json();
+};
+
+export const logout = async () => {
+  const response = await fetch("http://localhost:8009/api/v1/auth/logout/", {
+    method: "POST",
+    credentials: "include",
+  });
+  if (!response.ok) {
+    throw response;
+  }
+  return await response.json();
+};
