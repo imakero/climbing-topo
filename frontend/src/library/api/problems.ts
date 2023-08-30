e589df22ab4d2d3c9b7e7477e28cc9578a51479c
imakero <45946863+imakero@users.noreply.github.com>
2023-08-30 16:43:04 +0200

    add update problem api call

--- frontend/src/library/api/problems.ts
+++ frontend/src/library/api/problems.ts
@@ -1,4 +1,5 @@
 import { NewProblemData } from "@/app/admin/locations/[locationId]/components/NewProblemForm";
+import { EditProblemData } from "@/app/admin/problems/[problemId]/components/EditProblemForm";
 
 type ProblemPayload = NewProblemData & {
   location: number;
@@ -23,12 +24,27 @@ export const removeProblem = async (problemId: number) =>
     credentials: "include",
   });
 
-export const getProblems = async () =>
+export const getProblems = async (): Promise<WithId<Problem>[]> =>
   fetch(`http://localhost:8009/api/v1/problems/`, {
     cache: "no-cache",
   }).then((res) => res.json());
 
-export const getProblem = async (problemId: number) =>
+export const getProblem = async (problemId: number): Promise<WithId<Problem>> =>
   fetch(`http://localhost:8009/api/v1/problems/${problemId}/`, {
     cache: "no-cache",
   }).then((res) => res.json());
+
+export const updateProblem = async (
+  problemId: number,
+  data: Partial<EditProblemData>,
+) =>
+  fetch(`http://localhost:8009/api/v1/problems/${problemId}/`, {
+    method: "PATCH",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      ...data,
+    }),
+  });
