e5dc55aba752c04044877062391665c3e83bb870
imakero <45946863+imakero@users.noreply.github.com>
2023-08-30 12:55:12 +0200

    order problems by id

--- topo-api/app/climbs/models.py
+++ topo-api/app/climbs/models.py
@@ -29,6 +29,9 @@ class Problem(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        ordering = ["id"]
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
