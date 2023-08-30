a6f9b9cf6e6d8e86d5687ea7e76df9c7b2427aa8
imakero <45946863+imakero@users.noreply.github.com>
2023-08-30 13:32:15 +0200

    add basic admin navigation

--- frontend/src/app/admin/layout.tsx
+++ frontend/src/app/admin/layout.tsx
@@ -1,5 +1,13 @@
+import LinkButton from "@/components/LinkButton";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="container mx-auto min-h-full flex-grow">{children}</div>
+    <div className="container mx-auto min-h-full flex-grow">
+      <div className="my-4 flex flex-row space-x-4">
+        <LinkButton href="/admin/locations">Locations</LinkButton>
+        <LinkButton href="/admin/location-images">Location images</LinkButton>
+      </div>
+      {children}
+    </div>
   );
 }
