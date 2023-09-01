4dcc50e5e0f7c0af869a8e0ac62df93362dea7d2
imakero <45946863+imakero@users.noreply.github.com>
2023-09-01 14:21:26 +0200

    add basic user info to ascent serializer

--- topo-api/app/activities/serializers.py
+++ topo-api/app/activities/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
 from .models import Ascent
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class AscentSerializer(serializers.ModelSerializer):
+    class UserSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = User
+            fields = ["id", "username", "first_name", "last_name"]
+
+    user = UserSerializer(read_only=True)
+
     class Meta:
         model = Ascent
         fields = [
