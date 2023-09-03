cd141741833771479ff2402113925922fe92eb58
imakero <45946863+imakero@users.noreply.github.com>
2023-09-03 18:39:07 +0200

    component to list lines in location image

--- frontend/src/components/LocationImageProblems.tsx
+++ frontend/src/components/LocationImageProblems.tsx
@@ -1,33 +1,42 @@
-import { useRouter } from "next/navigation";
-import Button from "./Button";
-import { useEffect, useState } from "react";
-import { removeLine } from "@/library/api/lines";
+import Rating from "@/app/problems/[problemId]/components/Rating";
+import Link from "next/link";
+import React from "react";
 
 type LocationImageProblemsProps = {
   lines: LocationImageLine[];
-  onDelete: (lineId: number) => void;
+  highlight?: number;
+  setHighlight?: React.Dispatch<React.SetStateAction<number | undefined>>;
 };
 
-const LocationImageProblems = ({
+export default function LocationImageProblems({
   lines,
-  onDelete,
-}: LocationImageProblemsProps) => {
+  highlight = undefined,
+  setHighlight = () => {},
+}: LocationImageProblemsProps) {
   return (
-    <ol className="list-inside list-decimal">
-      {lines.map((line) => (
-        <li key={line.id}>
-          <div className="flex flex-row items-center justify-between">
-            <span>
-              {line.problem.name} ({line.problem.grade})
-            </span>
-            <div>
-              <Button onClick={(e) => onDelete(line.id)}>Delete</Button>
-            </div>
+    <ol className="flex list-inside list-decimal flex-col">
+      {lines.map(({ problem }, index) => (
+        <li
+          key={problem.id}
+          className={`${index === highlight ? "bg-slate-200" : ""}`}
+          onMouseEnter={() => setHighlight(index)}
+        >
+          <div className="inline-flex flex-col">
+            <Link
+              href={`/problems/${problem.id}`}
+              className="hover:text-teal-500"
+            >
+              <span className="font-bold">
+                {problem.name} ({problem.grade})
+              </span>
+            </Link>
+            {problem.rating !== null && <Rating rating={problem.rating} />}
+            {problem.description && (
+              <div className="text-sm">{problem.description}</div>
+            )}
           </div>
         </li>
       ))}
     </ol>
   );
-};
-
-export default LocationImageProblems;
+}
