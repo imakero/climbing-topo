cc03444c22d048a77a177579726dea6ac9aeb277
imakero <45946863+imakero@users.noreply.github.com>
2023-09-08 15:53:31 +0200

    link to problem page from problem admin

--- frontend/src/app/admin/problems/[problemId]/components/ProblemPage.tsx
+++ frontend/src/app/admin/problems/[problemId]/components/ProblemPage.tsx
@@ -1,5 +1,6 @@
 import { getTags } from "@/library/api/tags";
 import EditProblemForm from "./EditProblemForm";
+import Link from "next/link";
 
 type ProblemPageProps = {
   problem: WithId<Problem>;
@@ -9,7 +10,9 @@ const ProblemPage = async ({ problem }: ProblemPageProps) => {
   const tags = await getTags();
   return (
     <article>
-      <h1 className="text-2xl">{problem.name}</h1>
+      <Link href={`/problems/${problem.id}`} className="hover:text-teal-500">
+        <h1 className="text-2xl">{problem.name}</h1>
+      </Link>
       <EditProblemForm tags={tags} problem={problem} />
     </article>
   );
