810dd54f6a103dd4ddf636bb928a144fc65ee1a4
imakero <45946863+imakero@users.noreply.github.com>
2023-09-08 15:50:52 +0200

    add tagging of problems in admin

--- frontend/src/app/admin/problems/[problemId]/components/ProblemPage.tsx
+++ frontend/src/app/admin/problems/[problemId]/components/ProblemPage.tsx
@@ -1,16 +1,16 @@
-"use client";
-
+import { getTags } from "@/library/api/tags";
 import EditProblemForm from "./EditProblemForm";
 
 type ProblemPageProps = {
   problem: WithId<Problem>;
 };
 
-const ProblemPage = ({ problem }: ProblemPageProps) => {
+const ProblemPage = async ({ problem }: ProblemPageProps) => {
+  const tags = await getTags();
   return (
     <article>
       <h1 className="text-2xl">{problem.name}</h1>
-      <EditProblemForm problem={problem} />
+      <EditProblemForm tags={tags} problem={problem} />
     </article>
   );
 };
