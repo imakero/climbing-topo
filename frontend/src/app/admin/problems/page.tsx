9c8477d37bb84d912e89c9844e96bd3d9e71b69b
imakero <45946863+imakero@users.noreply.github.com>
2023-09-13 22:24:43 +0200

    add pagination in problems admin

--- frontend/src/app/admin/problems/page.tsx
+++ frontend/src/app/admin/problems/page.tsx
@@ -1,8 +1,22 @@
 import { getProblems } from "@/library/api/problems";
 import ProblemsPage from "./components/ProblemsPage";
 
-export default async function AdminProblems() {
-  const problems = await getProblems();
+export default async function AdminProblems({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+  let params: Record<string, string> = {};
+  if (searchParams["page"]) {
+    params["page"] = searchParams["page"] as string;
+  }
+  const { results, ...props } = await getProblems(params);
 
-  return <ProblemsPage problems={problems} />;
+  return (
+    <ProblemsPage
+      problems={results}
+      {...props}
+      page={searchParams["page"] ?? undefined}
+    />
+  );
 }
